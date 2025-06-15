e178e3bd9679d2960e085fc3e8349da348b2b16b
Deepak Meena <notwho53@gmail.com>
2025-06-15 17:28:50 +0530

    utils/systemd: use wl_event_loop_add_fd instead of deprecated Louvre fd API
    Signed-off-by: Deepak Meena <notwho53@gmail.com>

--- src/utils/Systemd.cpp
+++ src/utils/Systemd.cpp
@@ -39,19 +39,20 @@ Systemd::Systemd() noexcept
         goto failMatch;
     }
 
-    eventSource = G::compositor()->addFdListener(busFd, this, [](Int32, UInt32, void *data) -> Int32 {
-
-        Systemd *systemd { static_cast<Systemd*>(data) };
-
-        Int32 ret;
-
-        while ((ret = sd_bus_process(systemd->bus, NULL) > 0)) {}
-
-        if (ret < 0)
-            LLog::error("Failed to process bus: %s", strerror(-ret));
-
-        return 0;
-    });
+    eventSource = wl_event_loop_add_fd(
+        G::compositor()->eventLoop(),
+        busFd,
+        WL_EVENT_READABLE,
+        [](int, uint32_t, void *data) -> int {
+            Systemd *systemd = static_cast<Systemd*>(data);
+            int ret;
+            while ((ret = sd_bus_process(systemd->bus, nullptr)) > 0) {}
+            if (ret < 0)
+                LLog::error("Failed to process bus: %s", strerror(-ret));
+            return 0;
+        },
+        this
+    );
 
     restartXdgDesktopPortal();
     return;
@@ -65,7 +66,7 @@ failOpen:
 Systemd::~Systemd()
 {
     if (eventSource)
-        G::compositor()->removeFdListener(eventSource);
+        wl_event_source_remove(eventSource);
 
     if (bus)
         sd_bus_unref(bus);
